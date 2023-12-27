ca33cd092ad2231750208674604eb904818bdeb8
Natália Resina <resina.natalia@gmail.com>
2023-12-27 16:22:41 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using DotNet_Metodos_Construtores_DIO.Models;
+
+Pessoa pessoa1 = new Pessoa(nome: "Natália", sobrenome:"Resina");
+Pessoa pessoa2 = new Pessoa(nome: "André", sobrenome:"de Lima");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(pessoa1);
+cursoDeIngles.AdicionarAluno(pessoa2);
+cursoDeIngles.ListarAlunos();
+
+
+
